96e56d8c2582baa44c4ae93f1932f8c9996031d7
Tim <timchipperfield@gmail.com>
2016-10-05 22:42:15 +0100

    added join table

--- public_html/index.php
+++ public_html/index.php
@@ -13,25 +13,63 @@
     <title></title>
   </head>
   <body>
+    <div class=main_divider>
+    <div class ="table">
+        <?php
+          $SongQuery = "SELECT * FROM songs";
+          mysqli_query($db, $SongQuery) or die('Error connecting to db');
+          $SongResult = mysqli_query($db, $SongQuery);
 
-  <div class ="table_row">
-      <?php
+          $SongRow = mysqli_fetch_array($SongResult);
 
-        $query = "SELECT * FROM songs";
-        mysqli_query($db, $query) or die('Error connecting to db');
-        $result = mysqli_query($db, $query);
+          while ($SongRow = mysqli_fetch_array($SongResult)) {
+            echo '<div class="table_row">'
+            . '<div class="cell">' . $SongRow['song_name'] . '</div>'
+            . '<div class="cell">' . $SongRow['song_artist'] . '</div>'
+            . '<div class="cell">' . $SongRow['year'] . '</div>'
+            . '</div>';
 
-        $row = mysqli_fetch_array($result);
+          }
 
-        while ($row = mysqli_fetch_array($result)) {
-          echo '<div class="cell">' . $row['song_name'] . '</div>'
-          . '<div class="cell">' . $row['song_artist'] . '</div>'
-          . '<div class="cell">' . $row['year'] . '</div>';
 
-        }
+        ?>
+      </div>
 
-        mysqli_close($db);
-      ?>
+      <div class="table">
+        <?php
+          $SingerQuery = "SELECT * FROM singers";
+          mysqli_query($db, $SingerQuery) or die('Error connecting to db');
+          $SingerResult = mysqli_query($db, $SingerQuery);
+
+          $SingerRow = mysqli_fetch_array($SingerResult);
+
+          while ($SingerRow = mysqli_fetch_array($SingerResult)) {
+            echo '<div class="table_row">'
+            . '<div class="cell">' . $SingerRow['name'] . '</div>'
+            . '<div class="cell">' . $SingerRow['age'] . '</div>'
+            . '</div>';
+          }
+        ?>
+      </div>
+
+      <div class="table">
+        <?php
+          $JoinQuery = "SELECT * FROM songs INNER JOIN singers ON singers.name = songs.song_artist";
+          mysqli_query($db, $JoinQuery) or die('Error connecting to db');
+          $JoinResult = mysqli_query($db, $JoinQuery);
+
+          $JoinRow = mysqli_fetch_array($JoinResult);
+
+          while ($JoinRow = mysqli_fetch_array($JoinResult)) {
+            echo '<div class="table_row">'
+            . '<div class="cell">' . $JoinRow['name'] . '</div>'
+            . '<div class="cell">' . $JoinRow['age'] . '</div>'
+            . '<div class="cell">' . $JoinRow['year'] . '</div>'
+            . '</div>';
+          }
+          mysqli_close($db);
+        ?>
+      </div>
     </div>
   </body>
 </html>
